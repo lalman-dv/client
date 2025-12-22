8346a7869314d2f7eb0c527409ebe04090912dea
Lalman <lalmanchaudhary2@gmail.com>
2025-12-22 17:28:51 +0530

    feat:add router for resumeById

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -3,6 +3,8 @@ import protect from "../middlewares/authMiddleware.js";
 import {
   createResume,
   deleteResume,
+  getPublicResumeById,
+  getResumeById,
   updateResume,
 } from "../controllers/resumeController.js";
 import upload from "../configs/multer.js";
@@ -12,3 +14,7 @@ const resumeRouter = express.Router();
 resumeRouter.post("/create", protect, createResume);
 resumeRouter.put("/update", upload.single("image"), protect, updateResume);
 resumeRouter.delete("/delete/:resumeId", protect, deleteResume);
+resumeRouter.get("/get/:resumeId", protect, getResumeById);
+resumeRouter.get("/public/:resumeId", getPublicResumeById);
+
+export default resumeRouter;
