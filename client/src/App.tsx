c60f6aa80172781747fc559343bf89ebfe3c9bf6
Lalman <lalmanchaudhary2@gmail.com>
2025-12-23 20:55:01 +0530

    refactor: refactor(types): -enforce TypeScript type safety across Redux store, slices, and components
    -type safety across Redux store, slices, and components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,60 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import ResumeBuilder from "./components/resumebuilder/ResumeBuilder";
-import Login from "./pages/Login";
 import Preview from "./pages/Preview";
+import { useEffect } from "react";
+import api from "./configs/api";
+import { login, setLoading } from "./app/features/authSlice";
+import { useAppDispatch } from "./app/hooks";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
 
 const App = () => {
+  const dispatch = useAppDispatch();
+
+  const getUserData = async (): Promise<void> => {
+    const token = localStorage.getItem("token");
+    try {
+      if (token) {
+        const { data }: { data: { user?: User } } = await api.get(
+          "/api/users/data",
+          {
+            headers: { Authorization: token },
+          }
+        );
+
+        if (data.user) {
+          dispatch(login({ token, user: data.user }));
+        }
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    } catch (error) {
+      dispatch(setLoading(false));
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    void getUserData();
+  }, []);
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="app" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
-        </Route>
-        <Route path="view/:resumeId" element={<Preview />} />
-        <Route path="login" element={<Login />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="app" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+      </Route>
+      <Route path="view/:resumeId" element={<Preview />} />
+    </Routes>
   );
 };
 
--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,13 +1,27 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useSelector } from "react-redux";
+import type { RootState } from "../app/store";
+import Loader from "../components/Loader";
+import Login from "./Login";
 
 const Layout = () => {
+  const { user, loading } = useSelector((state: RootState) => state.auth);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Outlet />
-      </div>
+      {user ? (
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Outlet />
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 };
